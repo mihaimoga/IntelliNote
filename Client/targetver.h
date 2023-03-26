b7922d9561c6bf84282dfbd8fa7c9e57e623b083
Stefan-Mihai Moga <stefan-mihai.moga@nxp.com>
2023-03-26 13:20:04 +0300

    Updated copyright header.

---
+++ Client/targetver.h
@@ -0,0 +1,32 @@
+/* This file is part of IntelliNote application developed by Stefan-Mihai MOGA.
+
+IntelliNote is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Open
+Source Initiative, either version 3 of the License, or any later version.
+
+IntelliNote is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+IntelliNote. If not, see <http://www.opensource.org/licenses/gpl-3.0.html>*/
+
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface
+// (the "Fluent UI") and is provided only as referential material to supplement the
+// Microsoft Foundation Classes Reference and related electronic documentation
+// included with the MFC C++ library software.
+// License terms to copy, use or distribute the Fluent UI are available separately.
+// To learn more about our Fluent UI licensing program, please visit
+// https://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+#pragma once
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
